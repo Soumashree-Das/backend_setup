2ba336d963b1386e3176555557b756bdd7a79f4b
[soumashree das] <[soumshreedas2004@gmail.com]>
2024-08-15 10:39:46 +0530

    post not working

---
+++ src/routes/stock.routes.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import {
+    addStock,
+    getStockDetails,
+    getAllStock,
+    updateStock,
+    deleteStock  // Import the new controller function
+} from "../controllers/stock.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"
+
+const router = Router();
+
+// Route to get all stock items
+router.route("/")
+    .get(getAllStock)  // Add this line to fetch all stock items
+    .post(
+        upload.fields([
+            {
+                name : "photo" ,
+                maxCount : 1
+            }
+        ]),
+        addStock);   // Existing route to add a new stock item
+
+// Route to get stock details by `productId`
+router.route("/:productId")
+    .get(getStockDetails)
+    .patch(updateStock)
+    .delete(deleteStock);
+
+export default router;
